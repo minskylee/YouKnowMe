d7b55a9c69640effbb5ded89c7965aa000469a90
박진경 <wls0828rud@naver.com>
2022-08-10 10:00:30 +0900

    [S07P12D104-81] feat: admin 회원 관리 표 수정

--- FE/u-know-me/src/components/admin/AdminMember.vue
+++ FE/u-know-me/src/components/admin/AdminMember.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="admin-table">
     <div class="member">회원 관리</div>
-    <div class="member-table">
-      <table>
-        <thead>
-          <tr>
-            <th class="num">번호</th>
-            <th>유저 아이디</th>
-            <th>가입일</th>
-            <th>신고 누적 횟수</th>
-            <th>계정 상태</th>
-          </tr>
-        </thead>
-      </table>
-    </div>
-    <div class="member-table-content">
-      <table>
-        <tbody>
-          <tr>
-            <td class="num">번호</td>
-            <td>아이디</td>
-            <td>가입일</td>
-            <td>신고 누적 횟수</td>
-            <td>계정 상태</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table class="member-table">
+      <thead class="memeber-table-title">
+        <tr>
+          <th class="num">번호</th>
+          <th>유저 아이디</th>
+          <th>가입일</th>
+          <th>신고 누적 횟수</th>
+          <th>계정 상태</th>
+        </tr>
+      </thead>
+      <tbody class="member-table-content">
+        <tr>
+          <td class="num">번호</td>
+          <td>아이디</td>
+          <td>가입일</td>
+          <td>신고 누적 횟수</td>
+          <td>계정 상태</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -42,11 +36,13 @@ export default {
   font-weight: 550;
 }
 .member-table{
-  background-color: #f0f1ff;
   margin: 55px 0 0 0;
  }
+.memeber-table-title {
+  background-color: #f0f1ff;
+ }
 .member-table-content{
-  height:600px;
+  height: 80%;
   overflow-x:auto;
   margin-top: 0px;
   border: 1px solid #f0f1ff;
